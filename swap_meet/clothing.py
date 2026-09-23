2add2247a6b8ce9afc5793d1a8b13b45c35b9112
Shontai Blye <blye.career@gmail.com>
2026-09-23 18:14:30 -0400

    Finished Wave 5 together, all tests passing.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+        def __init__(self, condition=0, id=None, fabric="Unknown"):
+            Item.__init__(self, id=id, condition=condition)
+            self.fabric = fabric
+
+        def get_category(self):
+            return "Clothing"
+
+        def __str__(self):
+            return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -72,18 +72,3 @@ class Vendor:
 
 
 
-
-
-"""
-Instances of `Vendor` have an instance method named `swap_items`
-- It takes 3 arguments:
-- `swap_items` takes 3 arguments:
-    1. an instance of another `Vendor` (`other_vendor`), representing the friend that the vendor is swapping with
-    2. an instance of an `Item` (`my_item`), representing the item this `Vendor` instance plans to give
-    3. an instance of an `Item` (`their_item`), representing the item the friend `Vendor` plans to give
-  - The method removes `my_item` from this `Vendor`'s inventory, and adds it to the friend's inventory
-  - The method removes `their_item` from the other `Vendor`'s inventory, and adds it to this `Vendor`'s inventory
-  - The method returns `True`
-  - If this `Vendor`'s inventory doesn't contain `my_item` or the friend's inventory doesn't contain `their_item`, the method returns `False`
-
-"""
